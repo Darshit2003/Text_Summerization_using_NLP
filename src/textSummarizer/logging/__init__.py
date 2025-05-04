ebceba0d4227e4901e782362a031e54b967f01c0
Darshit Sheth <darshitsheth222@gmail.com>
2025-05-04 10:35:31 +0530

    Data Ingestion Done

---
+++ src/textSummarizer/logging/__init__.py
@@ -0,0 +1,19 @@
+import os,sys,logging
+from datetime import datetime as dt
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+log_dir = "logs"
+log_filepath = os.path.join(log_dir, f"{dt.now().strftime('%d_%m_%Y_%H_%M_%S')}.log")
+os.makedirs(log_dir, exist_ok=True)
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format=logging_str,
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger("textSummarizerLogger")
